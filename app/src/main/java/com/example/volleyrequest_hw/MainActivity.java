e030721fe2f7b79771b0f111f27fbf96a7e2f7b8
krylovam <krylova.maria.2629@gmail.com>
2020-12-16 00:27:50 +0300

    update app

--- app/src/main/java/com/example/volleyrequest_hw/MainActivity.java
+++ app/src/main/java/com/example/volleyrequest_hw/MainActivity.java
@@ -1,14 +1,7 @@
 package com.example.volleyrequest_hw;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,7 +9,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
@@ -28,62 +21,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button requestButton = (Button) findViewById(R.id.request_btn);
-        requestButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                makeRequest();
-            }
-        });
+
     }
-    private void makeRequest() {
-        // Instantiate the RequestQueue.
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)) {
-                // Show an explanation to the user
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 0);
-            }
-        }
-        else {
-            RequestQueue queue = Volley.newRequestQueue(this);
-            String url ="http://date.jsontest.com/";
 
-            // Request a string response from the provided URL.
-            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            String date;
-                            String mse;
-                            String time;
-                            try {
-                                JSONObject jsonResponse = new JSONObject(response);
-                                date = jsonResponse.getString("date");
-                                mse = jsonResponse.getString("milliseconds_since_epoch");
-                                time = jsonResponse.getString("time");
-                            } catch (JSONException e) {
-                                date = "Unable to parse date";
-                                mse = "Unable to parse mse";
-                                time = "Unable to parse time";
-                            }
-                            TextView dateTV = (TextView)findViewById(R.id.dateValue_tv);
-                            TextView mseTV = (TextView)findViewById(R.id.mseValue_tv);
-                            TextView timeTV = (TextView)findViewById(R.id.timeValue_tv);
-                            dateTV.setText(date);
-                            mseTV.setText(mse);
-                            timeTV.setText(time);
+    public void onclick(View v) {
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = "https://date.jsontest.com/";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String date;
+                        String mse;
+                        String time;
+                        try {
+                            date = response.getString("date");
+                            mse = response.getString("milliseconds_since_epoch");
+                            time = response.getString("time");
+                        } catch (JSONException e) {
+                            date = "Unable to parse date";
+                            mse = "Unable to parse mse";
+                            time = "Unable to parse time";
                         }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(getApplicationContext(),"Request failed: " + error.toString(), Toast.LENGTH_LONG).show();
-                }
-            });
+                        TextView dateTV = (TextView) findViewById(R.id.dateValue_tv);
+                        TextView mseTV = (TextView) findViewById(R.id.mseValue_tv);
+                        TextView timeTV = (TextView) findViewById(R.id.timeValue_tv);
+                        dateTV.setText(date);
+                        mseTV.setText(mse);
+                        timeTV.setText(time);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Request failed: " + error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
 
-            // Add the request to the RequestQueue.
-            queue.add(stringRequest);
-        }
+        request.setTag("TAG");
+        queue.add(request);
     }
 }
